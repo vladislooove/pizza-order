9e22bd4035e59a8a9caa0bfc5c2618b284e0f5be
fadetoblack1 <fadetoblack1@yandex.ru>
2018-02-25 21:05:41 +0200

    Added error handling

--- src/App.js
+++ src/App.js
@@ -23,7 +23,8 @@ class App extends Component {
 
         this.state = {
             formData: {
-                input: 'large pan veggie pizza plus pepperoni, heavy garlic, leave out the onions, well done with white sauce half mushrooms and extra olives and half light green peppers and pineapple'
+                input: 'large pan veggie pizza plus pepperoni, heavy garlic, leave out the onions, well done with white sauce half mushrooms and extra olives and half light green peppers and pineapple',
+                errorText: ''
             },
             result: null,
             isLoading: false,
@@ -84,91 +85,111 @@ class App extends Component {
     onInputChange(event) {
         this.setState({
             formData: {
-                input: event.target.value
+                input: event.target.value,
+                errorText: ''
             },
-        });       
+        });
+
+        if(!event.target.value.length) {
+            this.setState({
+                formData: {
+                    input: event.target.value,
+                    errorText: 'This field is required'
+                }
+            })
+        }
     }
     onFormSubmit(event) {
         event.preventDefault();
 
-        //set loading state to show loader
-        this.setState({ isLoading: true });
-        
-        const requestBody = new URLSearchParams();
-        requestBody.append('text', this.state.formData.input);
+        if(this.state.formData.input.length) {
+            
+            //set loading state to show loader
+            this.setState({ isLoading: true });
+            
+            const requestBody = new URLSearchParams();
+            requestBody.append('text', this.state.formData.input);
 
-        //configure axios request
-        const request = {
-            method: 'POST',
-            url: 'https://api.intouchposenterprise.com:1602/Convert.asmx/Xml',
-            data: requestBody,
-            headers: {
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                'Cache-Control': 'max-age=0',
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Upgrade-Insecure-Requests': '1'
-            },
-            responseType: 'document'
-        };
-        
-        axios(request)
-            .then((response) => {
-                if(response.data) {
+            //configure axios request
+            const request = {
+                method: 'POST',
+                url: 'https://api.intouchposenterprise.com:1602/Convert.asmx/Xml',
+                data: requestBody,
+                headers: {
+                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+                    'Cache-Control': 'max-age=0',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Upgrade-Insecure-Requests': '1'
+                },
+                responseType: 'document'
+            };
+            
+            axios(request)
+                .then((response) => {
+                    if(response.data) {
 
-                    //get string with xml content
-                    const xmlLikeResponse = response.data
-                        .querySelector('string')
-                        .textContent;
+                        //get string with xml content
+                        const xmlLikeResponse = response.data
+                            .querySelector('string')
+                            .textContent;
 
-                    //convert xmlLike string to native xml
-                    const xmlResponse = new DOMParser().parseFromString(xmlLikeResponse, 'text/xml');
-                    
-                    //convert xml response to json
-                    const jsonResponse = xml2json(xmlResponse.querySelector('Root'));
-                    
-                    this.setState({
-                        result: jsonResponse,
-                        isLoading: false,
-                        formData: {
-                            input: ''
-                        }
-                    });
-                }
-
-                // function from stackoverflow to parse xml
-                function xml2json(xml) {
-                    try {
-                        var obj = {};
-                        if (xml.children.length > 0) {
-                        for (var i = 0; i < xml.children.length; i++) {
-                            var item = xml.children.item(i);
-                            var nodeName = item.nodeName;
-                    
-                            if (typeof (obj[nodeName]) == "undefined") {
-                            obj[nodeName] = xml2json(item);
-                            } else {
-                            if (typeof (obj[nodeName].push) == "undefined") {
-                                var old = obj[nodeName];
-                    
-                                obj[nodeName] = [];
-                                obj[nodeName].push(old);
+                        //convert xmlLike string to native xml
+                        const xmlResponse = new DOMParser().parseFromString(xmlLikeResponse, 'text/xml');
+                        
+                        //convert xml response to json
+                        const jsonResponse = xml2json(xmlResponse.querySelector('Root'));
+                        
+                        this.setState({
+                            result: jsonResponse,
+                            isLoading: false,
+                            formData: {
+                                input: ''
                             }
-                            obj[nodeName].push(xml2json(item));
-                            }
-                        }
-                        } else {
-                        obj = xml.textContent;
-                        }
-                        return obj;
-                    } catch (e) {
-                        console.log(e.message);
+                        });
                     }
-                }                                      
+
+                    // function from stackoverflow to parse xml
+                    function xml2json(xml) {
+                        try {
+                            var obj = {};
+                            if (xml.children.length > 0) {
+                            for (var i = 0; i < xml.children.length; i++) {
+                                var item = xml.children.item(i);
+                                var nodeName = item.nodeName;
+                        
+                                if (typeof (obj[nodeName]) == "undefined") {
+                                obj[nodeName] = xml2json(item);
+                                } else {
+                                if (typeof (obj[nodeName].push) == "undefined") {
+                                    var old = obj[nodeName];
+                        
+                                    obj[nodeName] = [];
+                                    obj[nodeName].push(old);
+                                }
+                                obj[nodeName].push(xml2json(item));
+                                }
+                            }
+                            } else {
+                            obj = xml.textContent;
+                            }
+                            return obj;
+                        } catch (e) {
+                            console.log(e.message);
+                        }
+                    }                                      
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.setState({ isLoading: false })
+                })
+        } else {
+            this.setState({
+                formData: {
+                    input: this.state.formData.input,
+                    errorText: 'This field is required'
+                }
             })
-            .catch((error) => {
-                console.log(error);
-                this.setState({ isLoading: false })
-            })
+        }
     }
     render() {
         return (
@@ -206,6 +227,7 @@ class App extends Component {
                                         floatingLabelFixed={true}                            
                                         rows={2}
                                         value={this.state.formData.input}
+                                        errorText={this.state.formData.errorText}                                        
                                         onChange={this.onInputChange.bind(this)}
                                     />
                                     <RaisedButton label="Extract" 
